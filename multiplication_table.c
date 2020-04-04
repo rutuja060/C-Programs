646c766b21b37f78db22aca4fc47d8f0d02167fc
rutuja060 <59307860+rutuja060@users.noreply.github.com>
2020-04-04 19:55:23 +0530

    Add files via upload

---
+++ multiplication_table.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+    int i,a,result;
+    printf("enter a no");
+    scanf("%d",&a);
+    for(i=0;i<=10;i++)
+    {
+
+    result=a*i;
+    printf("%d \n",result);
+    }
+}
---
+++ odd_number.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main(){
+int a;
+for (a=0;a<=100;a++)
+{
+    a%2!=0;
+    printf("%d \n",a);
+    
+}
+}
